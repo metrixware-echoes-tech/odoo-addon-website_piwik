176f70b3642e29d8ad56a7f69082dfc811e6cade
Florent Poinsaut <florent.poinsaut@echoes-tech.com>
2014-12-01 09:40:29 +0100

    First Release

---
+++ __openerp__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'Piwik for Website',
+    'version': '0.1',
+    'category': 'Website',
+    'complexity': "easy",
+    'description': """
+Piwik for Website.
+=================
+
+Collects web application usage of website with Piwik.
+    """,
+    'author': 'Echoes Technologies SAS',
+    'website': 'https://www.echoes-tech.com',
+    'depends': ['website'],
+    'data': [
+        'views/website_piwik.xml',
+    ],
+    'installable': True,
+    'active': False,
+}
